53db7cccabd598d9a93290d7a88671aaa2a598d7
Mateusz Szebestik <szebestik@gmail.com>
2024-04-09 00:43:42 +0200

    Refactored the rendering when data-theme is missing

--- src/app/layout/containers/default-layout/default-layout.component.ts
+++ src/app/layout/containers/default-layout/default-layout.component.ts
@@ -1,7 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-import { ThemeService } from '@core/services';
 
 import { HeaderComponent } from '@layout/components';
 
@@ -12,8 +10,4 @@ import { HeaderComponent } from '@layout/components';
   templateUrl: './default-layout.component.html',
   styleUrl: './default-layout.component.scss',
 })
-export class DefaultLayoutComponent {
-  themeService = inject(ThemeService);
-
-  theme = this.themeService.theme;
-}
+export class DefaultLayoutComponent {}
